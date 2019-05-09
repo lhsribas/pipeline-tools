0ac2f3a178ca2263f27fe4bc998e5f235154703c
lhsribas <lribas@redhat.com>
2019-05-09 18:08:02 -0300

    update

--- vars/javaSharedPipeline.groovy
+++ vars/javaSharedPipeline.groovy
@@ -39,6 +39,8 @@ def call(body) {
         //def runnerSonar = (pipelineParams.sonarEnv != "") ? false : true
         def rollout = true
 
+        def pomProccess = load 'pomProccess.groovy'
+
     pipeline {
 
         agent 
@@ -60,156 +62,19 @@ def call(body) {
                 }
             }
 
-           /* switch(pipelineParams.environment) 
+           switch(pipelineParams.environment) 
             {
-                case "dev":
+                case "DEV":
 
-                    stage('Compile')
+                    stage("example")
                     {
                         steps
-                        {    
-                            withMaven( maven: "${pipelineParams.maven}", mavenSettingsConfig: "${pipelineParams.mavenSettingsConfig}") 
-                            {
-                                sh "mvn clean compile -DskipTests"
-                            }
-                        }
-                    }
-
-                    stage('Test') 
-                    {
-                        steps {
-                            withMaven( maven: "${pipelineParams.maven}", mavenSettingsConfig: "${pipelineParams.mavenSettingsConfig}") 
-                            {
-                                sh "mvn test"
-                            }
-                        }
-                    }
-                
-                    stage('Package') 
-                    {
-                        steps {
-                            withMaven( maven: "${pipelineParams.maven}", mavenSettingsConfig: "${pipelineParams.mavenSettingsConfig}") 
-                            {
-                                sh "mvn package -DskipTests"
-                            }
-                        }
-                    }
-
-                    if(runnerSonar){
-                        stage('Sonar')
                         {
-                            steps
-                            {
-                                withMaven( maven: "${pipelineParams.maven}", mavenSettingsConfig: "${pipelineParams.mavenSettingsConfig}") 
-                                {
-                                    withSonarQubeEnv("${pipelineParams.sonarEnv}") 
-                                    {
-                                        sh 'mvn sonar:sonar'
-                                    }
-                                } 
-                            }
-                        }
-
-                        stage('Quality Gate') 
-                        {
-                            steps 
-                            {
-                                script{
-                                    timeout(time: 5, unit: 'MINUTES') 
-                                    {
-                                        def _qg = sonarQualityGatesProccess.qualityGateResults("target/sonar/report-task.txt")
-                                        
-                                        if (_qg.status != 'OK') 
-                                        {
-                                            error "Please review the quality of your code.\nstatus of analisys: ${qg.status}"
-                                        }
-                                    }	
-                                }
-                            }
-                        }
-                    } 
-                    else 
-                    {
-                        info "Sonar quality gate is disabled"
-                    }
-
-                    stage('Create Image Builder') 
-                    {
-                        steps 
-                        {
-                            script 
-                            {
-                                echo "Create Image Builder"
-                                openshiftProccess.createImageBuilder(pipelineParams)
-                            }
+                            echo "${pomProccess.getArtifactIdFromPom}"
                         }
                     }
-
-                    stage('Start Image Builder') 
-                    {
-                        steps 
-                        {
-                            script 
-                            {
-                                echo "Start Image Builder!"
-                                openshiftProccess.startBuild(pipelineParams)
-                            }
-                        }
-                    }
-
-                    stage("Tag Image to ${pipelineParams.environment}") 
-                    {
-                        steps 
-                        {
-                            script 
-                            {
-                                echo "Tag Image!"
-                                openshiftProccess.tagImage(pipelineParams)
-                            }
-                        }
-                    }
-
-                    stage("Create ${pipelineParams.environment}") {
-                        when
-                        {
-                            expression 
-                            {
-                                echo "Verify exists Deployment Config!"
-                                return  openshiftProccess.vefirifyExistsDeploymentConfig(pipelineParams);
-                            }
-                        }
-                        
-                        steps 
-                        {
-                            script 
-                            {
-                                echo "Create Deployment Config if not exists!"
-                                rollout = openshiftProccess.createDeploymentConfig(pipelineParams)
-                            }
-                        }
-                    }
-
-                    stage('Rollout Dev')
-                    {
-                        when
-                        {
-                            expression 
-                            {
-                                return rollout
-                            }
-                        }
-                        
-                        steps{
-                            script {
-                                openshiftProccess.rolloutVersion(pipelineParams)
-                            }
-                        }
-                    }
-
                 break;
             }
-            */
-            
         }
     }
 }
