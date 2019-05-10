6dfb6983982651bac723f4210f3c6186e3407f1f
lhsribas <lribas@redhat.com>
2019-05-10 16:15:01 -0300

    update

--- vars/mavenProccess.groovy
+++ vars/mavenProccess.groovy
@@ -1,13 +1,11 @@
 #!/usr/bin/env groovy
 
-
 /*
  *
  */
 def execCleanProject(def _maven, def _settings){
-    //withMaven( maven: "${_maven}", mavenSettingsConfig: "${_settings}") {
-      //  sh "mvn clean"
-    //}
-
-    echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> ${_maven} >>>>>>>>>> ${_settings}"
+    echo ">>>> maven ::: ${_maven} >>>>> maven_settings:::  ${_settings}"
+    withMaven( maven: "${_maven}", mavenSettingsConfig: "${_settings}") {
+        sh "mvn clean"
+    }
 }
