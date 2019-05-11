3c920e7ce5d4eab0fe39da60be58f010b46ab33f
lhsribas <lribas@redhat.com>
2019-05-11 11:41:17 -0300

    Change the name of script javaSharedPipeline to javaMavenSharedPipeline and add description of doc.

--- commit.sh
+++ commit.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-git commit -am "update"
-git push
+echo $1
+
+if [ !-z $1 ]; then 
+
+  git commit -am "$1"
+  git push
+
+else
+
+  echo "Please provide a message to execute a commit"
+
+fi;
